b8184afac009da1eb21b4b18c8a3c417d1f383d2
MontoyaAndres <andresmontoyafcb@gmail.com>
2019-11-02 14:55:27 -0500

    getting started

---
+++ TailwindCSS/tailwind-nextjs/README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=7phZqghhho0&list=WL&index=61&t=0s (7:00)
---
+++ TailwindCSS/tailwind-nextjs/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style.css";
+
+function index() {
+  return <header style=""></header>;
+}
+
+export default index;
